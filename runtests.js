2ea7b9bef81f52043b10482ea94747a68d301522
Kendrick Shaw <kms15@case.edu>
2011-12-28 20:11:16 +0000

    added node.js test runner

---
+++ runtests.js
@@ -0,0 +1,14 @@
+var requirejs = require('requirejs');
+
+// standard requirejs server initialization to choose the default require call
+requirejs.config({
+    nodeRequire: require
+});
+
+requirejs(
+    ['tools/test','test/basearray_test.js'], 
+    function(test, basearray_test) {
+    "use strict";
+
+    test.runTests(test.consoleReporter);
+});
