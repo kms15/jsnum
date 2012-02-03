aa030f59038c29e89254032223271f986244e656
Kendrick Shaw <kms15@case.edu>
2012-02-03 00:25:07 +0000

    add and use assert.AssertionError

--- tools/assert.js
+++ tools/assert.js
@@ -1,14 +1,38 @@
 /*global define */
+/*
+ * Implements assert framework, based on the CommonJS Unit Testing proposal 1.0
+ * found at wiki.commonjs.org/wiki/Unit_Testing/1.0 
+ */
+
 define([], function () {
     "use strict";
-    var assert = {};
+    var assert = {},
+        globalObj = (function () { return this; }());
+
+    assert.AssertionError = function (init) {
+        if (this === globalObj) {
+            // called without new
+            return new assert.AssertionError(init);
+        } else {
+            this.message = init.message;
+            this.expected = init.expected;
+            this.actual = init.actual;
+        }
+    };
+    assert.AssertionError.prototype = Object.create(Error);
+    assert.AssertionError.prototype.toString = function () {
+        return "Assertion failed: " + this.message +
+            (this.expected ? "  Expected: " + this.expected : "") +
+            (this.actual ? "  Actual: " + this.actual : "");
+    };
 
     assert.strictEqual = function (actual, expected, message) {
         if (actual !== expected) {
-            throw new Error("Assertion failed: " +
-                (message ? message + '; ' : '') +
-                "expected exactly " + expected + " but value was " +
-                actual + ".");
+            throw new assert.AssertionError({
+                message : message,
+                actual : actual,
+                expected : expected
+            });
         }
     };
 
